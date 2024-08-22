c8b144f51b5eb47bdbeaf79c3f3c183bcc4aa92d
JSampereVázquez <72076376+jsamperevazquez@users.noreply.github.com>
2024-08-22 17:15:34 +0200

    Reto(terminado)

---
+++ python/00-SINTAXIS/01-Sintaxis.py
@@ -0,0 +1,20 @@
+"""
+EJERCICIO:
+ - Crea un comentario en el código y coloca la URL del sitio web oficial del
+   lenguaje de programación que has seleccionado.
+ - Representa las diferentes sintaxis que existen de crear comentarios
+   en el lenguaje (en una línea, varias...).
+- Crea una variable (y una constante si el lenguaje lo soporta).
+- Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+"""
+## URL official Site: https://docs.python.org/es/3/
+var = 123
+CONST = "CONSTANTE"
+cadena = "Esto es una cadena de texto"
+booleano = True
+entero = 25
+_float_ = 2.5
+language = "Python"
+print("Hola " + language)
